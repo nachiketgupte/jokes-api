0b5d96ffd43cc81674362a5060eb37d342b4de7b
unknown <nachiketgupte8@gmail.com>
2024-02-08 10:56:03 +0530

    Added delet method

--- index.js
+++ index.js
@@ -75,6 +75,20 @@ app.patch("/jokes", (req,res) => {
 
 //7. DELETE Specific joke
 
+app.delete("/jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id);
+  const jokeIndex = jokes.findIndex((joke) => joke.id === id);
+  if(jokeIndex != -1) {
+    jokes.splice(jokeIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+    .status(404)
+    .json({error: `Joke with ID ${id} not found. No jokes were deleted`});
+  }
+  
+})
+
 //8. DELETE All jokes
 
 app.listen(port, () => {
